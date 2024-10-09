0f4f60a59c05f8393451e80816de836cd2a54d90
Lars Ivar Hatledal <laht@ntnu.no>
2024-10-09 12:02:58 +0200

    move Simulation into Simulation.hpp

---
+++ include/Simulation.hpp
@@ -0,0 +1,38 @@
+
+
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP
+
+#include "Logger.hpp"
+
+class Simulation {
+public:
+    void init() {
+        if (logger_) {
+            logger_->log("Simulation initialized!");
+        }
+    }
+
+    void step(double dt) {
+        t_ += dt;
+        if (logger_) {
+            logger_->log("t=" + std::to_string(t_));
+        }
+    }
+
+    void terminate() {
+        if (logger_) {
+            logger_->log("Simulation terminated!");
+        }
+    }
+
+    void setLogger(std::unique_ptr<Logger> logger) {
+        logger_ = std::move(logger);
+    }
+
+private:
+    float t_ = 0;
+    std::unique_ptr<Logger> logger_;
+};
+
+#endif //SIMULATION_HPP
--- main.cpp
+++ main.cpp
@@ -1,38 +1,4 @@
-#include <iostream>
-
-#include "Logger.hpp"
-
-
-class Simulation {
-public:
-    void init() {
-        if (logger_) {
-            logger_->log("Simulation initialized!");
-        }
-    }
-
-    void step(double dt) {
-        t_ += dt;
-        if (logger_) {
-            logger_->log("t=" + std::to_string(t_));
-        }
-    }
-
-    void terminate() {
-        if (logger_) {
-            logger_->log("Simulation terminated!");
-        }
-    }
-
-    void setLogger(std::unique_ptr<Logger> logger) {
-        logger_ = std::move(logger);
-    }
-
-private:
-    float t_ = 0;
-    std::unique_ptr<Logger> logger_;
-};
-
+#include "Simulation.hpp"
 
 int main() {
     Simulation sim;
